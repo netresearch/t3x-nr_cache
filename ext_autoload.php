7458ae21c06617c914ee79289d19ef5a4f8f5a87
Sebastian Mendel <sebastian.mendel@netresearch.de>
2014-07-07 10:35:34 +0200

    TYPO-1899: fix auto laod information - remove superfluous leading backslash in class name - fixes auto loading not working

--- ext_autoload.php
+++ ext_autoload.php
@@ -19,11 +19,11 @@ defined('TYPO3_MODE') or die('Access denied.');
 $strPath = t3lib_extMgm::extPath('nr_cache');
 
 return array(
-    '\netresearch\cache\backend_couchbase'
+    'netresearch\cache\backend_couchbase'
         => $strPath . 'src/Netresearch/Cache/Backend/Couchbase.php',
-    '\netresearch\cache\frontend_functionresult'
+    'netresearch\cache\frontend_functionresult'
         => $strPath . 'src/Netresearch/Cache/Frontend/FunctionResult.php',
-    '\netresearch\cache\streamwrapper'
+    'netresearch\cache\streamwrapper'
         => $strPath . 'src/Netresearch/Cache/StreamWrapper.php',
 );
 ?>
