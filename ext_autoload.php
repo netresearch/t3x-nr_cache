1182263138b802f60cbf250b24ba9c1b83e6d1e0
Sebastian Mendel <sebastian.mendel@netresearch.de>
2014-08-19 13:01:08 +0200

    TYPO-2741: outsource session handling into own extension nr_session

--- ext_autoload.php
+++ ext_autoload.php
@@ -18,10 +18,16 @@ defined('TYPO3_MODE') or die('Access denied.');
 
 $strPath = t3lib_extMgm::extPath('nr_cache');
 return array(
-    'netresearch\cache\backend_couchbaseksdfjsdfjdgfjkdgfjsda'
+    'netresearch\cache\backend_couchbase'
         => $strPath . 'src/Netresearch/Cache/Backend/Couchbase.php',
+    'netresearch\cache\backend_redis'
+        => $strPath . 'src/Netresearch/Cache/Backend/Redis.php',
+    'netresearch\cache\frontend_code'
+        => $strPath . 'src/Netresearch/Cache/Frontend/Code.php',
     'netresearch\cache\frontend_functionresult'
         => $strPath . 'src/Netresearch/Cache/Frontend/FunctionResult.php',
+    'netresearch\cache\frontend_string'
+        => $strPath . 'src/Netresearch/Cache/Frontend/String.php',
     'netresearch\cache\streamwrapper'
         => $strPath . 'src/Netresearch/Cache/StreamWrapper.php',
 );
