ac13ed514d09bf2a78554e3849ea3d9d1403be22
Sebastian Mendel <sebastian.mendel@netresearch.de>
2014-08-26 13:45:01 +0200

    Version: 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "nr_cache".
  *
- * Auto generated 21-08-2014 10:34
+ * Auto generated 26-08-2014 13:45
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'state' => 'alpha',
-	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:11:{s:9:"build.xml";s:4:"64f8";s:9:"ChangeLog";s:4:"a376";s:16:"ext_autoload.php";s:4:"8ec0";s:10:"README.rst";s:4:"e902";s:18:"doc/ide_helper.php";s:4:"02db";s:39:"src/Netresearch/Cache/StreamWrapper.php";s:4:"5c42";s:43:"src/Netresearch/Cache/Backend/Couchbase.php";s:4:"05e9";s:39:"src/Netresearch/Cache/Backend/Redis.php";s:4:"88c4";s:39:"src/Netresearch/Cache/Frontend/Code.php";s:4:"df38";s:49:"src/Netresearch/Cache/Frontend/FunctionResult.php";s:4:"f35b";s:41:"src/Netresearch/Cache/Frontend/String.php";s:4:"9643";}',
+	'version' => '1.2.0',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"build.xml";s:4:"1e05";s:9:"ChangeLog";s:4:"470d";s:16:"ext_autoload.php";s:4:"662b";s:10:"README.rst";s:4:"6edc";s:18:"doc/ide_helper.php";s:4:"02db";s:39:"src/Netresearch/Cache/StreamWrapper.php";s:4:"b62c";s:43:"src/Netresearch/Cache/Backend/Couchbase.php";s:4:"9fba";s:39:"src/Netresearch/Cache/Backend/Redis.php";s:4:"3d0b";s:39:"src/Netresearch/Cache/Frontend/Code.php";s:4:"89b6";s:49:"src/Netresearch/Cache/Frontend/FunctionResult.php";s:4:"e3a1";s:41:"src/Netresearch/Cache/Frontend/String.php";s:4:"cdf2";}',
 );
 
 ?>
