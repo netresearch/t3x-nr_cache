b88b0aacb1b5bc739e3108d692cd81f118c92581
Tobias Hein <tobias.hein@netresearch.de>
2014-11-20 11:41:48 +0100

    Version: 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "nr_cache".
  *
- * Auto generated 06-11-2014 13:00
+ * Auto generated 20-11-2014 11:41
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'state' => 'stable',
-	'version' => '2.0.0',
-	'_md5_values_when_last_written' => 'a:15:{s:9:"build.xml";s:4:"1e05";s:9:"ChangeLog";s:4:"5152";s:16:"ext_autoload.php";s:4:"662b";s:12:"ext_icon.gif";s:4:"a459";s:10:"README.rst";s:4:"cdda";s:13:"doc/clear.lua";s:4:"467e";s:18:"doc/ide_helper.php";s:4:"02db";s:12:"doc/scan.lua";s:4:"b1da";s:35:"src/Netresearch/Cache/Exception.php";s:4:"95f1";s:39:"src/Netresearch/Cache/StreamWrapper.php";s:4:"b62c";s:43:"src/Netresearch/Cache/Backend/Couchbase.php";s:4:"9fba";s:39:"src/Netresearch/Cache/Backend/Redis.php";s:4:"5644";s:39:"src/Netresearch/Cache/Frontend/Code.php";s:4:"89b6";s:49:"src/Netresearch/Cache/Frontend/FunctionResult.php";s:4:"88ef";s:41:"src/Netresearch/Cache/Frontend/String.php";s:4:"cdf2";}',
+	'version' => '2.1.0',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"build.xml";s:4:"1e05";s:9:"ChangeLog";s:4:"1d81";s:16:"ext_autoload.php";s:4:"d81e";s:12:"ext_icon.gif";s:4:"a459";s:10:"README.rst";s:4:"2295";s:13:"doc/clear.lua";s:4:"467e";s:18:"doc/ide_helper.php";s:4:"02db";s:12:"doc/scan.lua";s:4:"b1da";s:35:"src/Netresearch/Cache/Exception.php";s:4:"95f1";s:39:"src/Netresearch/Cache/StreamWrapper.php";s:4:"b62c";s:43:"src/Netresearch/Cache/Backend/Couchbase.php";s:4:"9fba";s:39:"src/Netresearch/Cache/Backend/Redis.php";s:4:"5644";s:39:"src/Netresearch/Cache/Frontend/Code.php";s:4:"89b6";s:49:"src/Netresearch/Cache/Frontend/FunctionResult.php";s:4:"88ef";s:46:"src/Netresearch/Cache/Frontend/NonVolatile.php";s:4:"e537";s:41:"src/Netresearch/Cache/Frontend/String.php";s:4:"cdf2";}',
 );
 
 ?>
